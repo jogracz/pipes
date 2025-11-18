c424c75397e58f3ca86b01c1702973f4861c7100
Joanna Graczyk <joanna.graczyk1@gmail.com>
2025-11-18 13:08:58 +0100

    Add webpack server and html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,20 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     entry: "./src/index.ts",
     devtool: "inline-source-map",
+    devServer: {
+        static: "./dist",
+    },
+    plugins: [new HtmlWebpackPlugin({ title: "Pipes Game" })],
+    optimization: {
+        runtimeChunk: "single",
+    },
     output: {
-        filename: "bundle.js",
+        filename: "[name].bundle.js",
         path: path.resolve(__dirname, "dist"),
+        clean: true,
     },
     mode: "development",
     module: {
